e4530e64b23143549e8b60fb36b4e62b15ca07f2
iwoty <iwo.tyszkowski@gmail.com>
2017-04-25 13:27:28 +0200

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -4,9 +4,10 @@ from collections import OrderedDict     # for sorting of dictionary keys
 while True:
 
     my_dict = {}
-    with open('data.csv', 'r') as f:
-        my_dict = dict(csv.reader(f))
-    print(my_dict)
+    with open('data.csv', mode='r') as open_data:  # Opening a file in read mode
+        reader = csv.reader(open_data, delimiter=',')   # delimiter tells us about separator
+        for row in reader:
+            my_dict[row[0]] = (row[1], row[2])
 
     try:
         print('/----------------------------------------------------------------------\ ')
@@ -25,18 +26,16 @@ while True:
                 print("Explanation:", my_dict.get(search)[0])   # we are taking  first element
                 print("Source:", my_dict.get(search)[1])
             else:
-                print("Sadly there is no such definition :( But you can add that to dictioary! ( ͡° ͜ʖ ͡°)")
+                print("Sadly there is no such definition :( But you can add one to dictionary! ( ͡° ͜ʖ ͡°)")
 
         elif menu == 2:
-            new_element = {}
             new_definition = input("Enter name of definition(key): ")
             new_explanation = input("Enter explanation: ")
             new_source = input("Enter source: ")
-            new_element[new_definition] = (new_explanation, new_source)
-            print(new_element)
-            with open('data.csv', 'w') as f:
-                csv.writer(f).writerows(new_element.items())
-"''ZAPISUJE Z CUDZYSLOWAMI NAOKOLO TUPLA i nadpisuje ciagle te same dane zamiast przejsc do kolejnej linii"''
+
+            with open('data.csv', mode='a') as add_data:  # Opening a file in append mode
+                append_csv = csv.writer(add_data, delimiter=',')
+                append_csv.writerow([new_definition, new_explanation, new_source])
 
         elif menu == 3:
             my_dict = OrderedDict(sorted(my_dict.items()))  # sorting of elements in dictionary
