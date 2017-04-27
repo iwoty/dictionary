2f404bd668a257faf2b9a0846140e74348759678
iwoty <iwo.tyszkowski@gmail.com>
2017-04-27 12:50:11 +0200

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,5 @@
 import csv
-from collections import OrderedDict     # for sorting of dictionary keys
+from collections import OrderedDict     # to sort dictionary keys
 
 while True:
     try:
@@ -11,6 +11,7 @@ while True:
     except FileNotFoundError:
         print("Data.csv file not found :(")
         exit()
+
     try:
         print('/----------------------------------------------------------------------\ ')
         print(' Dictionary for a little programmer:\n\
@@ -20,9 +21,10 @@ while True:
         4) show available definitions by first letter of appellation\n\
         0) exit')
         print('\----------------------------------------------------------------------/ ')
-        menu = int(input("Choose option from menu: "))
 
-        if menu == 1:
+        menu_choice = int(input("Choose option from menu: "))
+
+        if menu_choice == 1:
             search = input("What definition are you looking for? ")
             if search in my_dict:   # dictionary membership test
                 print("Explanation:", my_dict.get(search)[0])   # we are taking  first element
@@ -30,7 +32,7 @@ while True:
             else:
                 print("Sadly there is no such definition :( But you can add one to dictionary! ( ͡° ͜ʖ ͡°)")
 
-        elif menu == 2:
+        elif menu_choice == 2:
             new_definition = input("Enter name of definition(key): ")
             new_explanation = input("Enter explanation: ")
             new_source = input("Enter source: ")
@@ -39,19 +41,21 @@ while True:
                 append_csv = csv.writer(add_data, delimiter=',')
                 append_csv.writerow([new_definition, new_explanation, new_source])
 
-        elif menu == 3:
+        elif menu_choice == 3:
             my_dict = OrderedDict(sorted(my_dict.items()))  # sorting of elements in dictionary
             for key in my_dict:     # printing only the keys
                 print(key)
 
-        elif menu == 4:
+        elif menu_choice == 4:
             my_dict = OrderedDict(sorted(my_dict.items()))
             letter_choice = input("Enter first letter of definition: ")
+            if len(letter_choice) > 1:
+                print('Is that only FIRST letter of definition?')
             for key in my_dict:
                 if key[0].upper() == letter_choice.upper():
                     print(key)
 
-        elif menu == 0:
+        elif menu_choice == 0:
             break
         else:   # dupa test
             print('Choose proper menu number!')
